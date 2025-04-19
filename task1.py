59abb46b2dfaa349780f5ba1030645a24b270196
avadheshm21 <mandlik.mandlik@gmail.com>
2025-04-19 23:56:44 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+#Assignment 1
+
+#Task 1: Perform Basic Mathematical Operations
+
+a= int(input('Enter the first number:'))
+b= int(input('Enter the second number:'))
+
+add=a+b
+sub=a-b
+m=a*b
+d=a/b
+
+print('Addition:', add)
+print('Subtraction:', sub)
+print('Multiplication:', m)
+print('Division:', d)
---
+++ task2.py
@@ -0,0 +1,6 @@
+#Task 2: Create a Personalized Greeting
+
+fn=input('Enter your first name: ')
+ln=input('Enter your last name: ')
+
+print('Hello,',fn,ln,'! Welcome to the Python program.' )
